3d3cf722c72ec10889508f8fb64c837fae800634
naz6185 <52354276+naz6185@users.noreply.github.com>
2019-08-01 02:18:43 +0530

    Update WhenCreatingANewGame.java

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.webtests.controllers;
 
-//import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
